ec41716c061cd25f3a58a3b07d1d4bb88a7862ca
phcharbonneau <phcharbonneau@hotmail.com>
2020-04-05 14:39:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(
-   name='server',
-   version='1.0',
-   description='A useful module',
-   author='Man Foo',
-   author_email='phcharbonneau@hotmail.com',
-   packages=['server'],  #same as name
-   install_requires=['bar', 'greek'], #external packages as dependencies
+
+setup (
+    name             = "testapp",
+    version          = "0.1",
+    description      = "Example application to be deployed.",
+    packages         = find_packages(),
+    install_requires = ["gunicorn"],
 )
